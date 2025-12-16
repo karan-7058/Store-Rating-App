329d2c23404899c374f6db93da6d9f4d7edf2781
karan-7058 <aghavkaran164@gmail.com>
2025-12-16 18:14:26 +0530

    cleanup

--- models/Rating.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const ratingSchema = new mongoose.Schema({
-  store: { type: mongoose.Schema.Types.ObjectId, ref: "Store" },
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  value: Number
-});
-
-module.exports = mongoose.model("Rating", ratingSchema);
