3ed50f564831da97e781599271d5c7e3cfcb3809
karan-7058 <aghavkaran164@gmail.com>
2026-01-11 19:08:07 +0530

    corrected a spelling mistakes

--- client/StoreRating/src/pages/Home.jsx
+++ client/StoreRating/src/pages/Home.jsx
@@ -16,7 +16,7 @@ export default function Home() {
       {stores.map(store => (
         <div key={store.id} style={{ padding: 10, borderBottom: "1px solid #ddd" }}>
           <h3>
-            <Link to={`/store/${store.id}`}>{store.name}</Link>
+            <Link to={`/stores/${store.id}`}>{store.name}</Link>
           </h3>
           <p>{store.description}</p>
           <p>⭐ Avg Rating: {store.avg_score}</p>
