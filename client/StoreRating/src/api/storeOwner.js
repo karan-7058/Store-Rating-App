b86b921147a4435474313e1a66c0da480441781b
karan-7058 <aghavkaran164@gmail.com>
2026-01-11 18:57:22 +0530

    created a create store functionality for storeowner

--- client/StoreRating/src/api/storeOwner.js
+++ client/StoreRating/src/api/storeOwner.js
@@ -2,7 +2,7 @@ const API = "http://localhost:3000";
 
 // Get stores created by owner
 export async function getOwnerStores() {
-  const res = await fetch(`${API}/storeowner/stores`, {
+  const res = await fetch(`${API}/owner/stores`, {
     credentials: "include",
   });
   return res.json();
@@ -10,8 +10,22 @@ export async function getOwnerStores() {
 
 // Get ratings for a specific store owned by owner
 export async function getOwnerStoreRatings(id) {
-  const res = await fetch(`${API}/storeowner/stores/${id}/ratings`, {
+  const res = await fetch(`${API}/owner/stores/${id}/ratings`, {
     credentials: "include",
   });
   return res.json();
 }
+
+export async function createOwnerStore(storeData) {
+  const res = await fetch("http://localhost:3000/owner/stores", {
+    method: "POST",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(storeData),
+  });
+
+  return res.json();
+}
+
