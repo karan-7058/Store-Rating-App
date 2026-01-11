d662ec688463cd9b9e68bec6d08efd11b616fd1d
karan-7058 <aghavkaran164@gmail.com>
2026-01-11 19:07:28 +0530

    added a welcome name after login

--- client/StoreRating/src/pages/OwnerDashboard.jsx
+++ client/StoreRating/src/pages/OwnerDashboard.jsx
@@ -1,17 +1,27 @@
 import { Link } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
 
 export default function OwnerDashboard() {
+  const { user } = useAuth();
+
   return (
     <div style={{ padding: "20px" }}>
       <h1>Store Owner Dashboard</h1>
 
-      <p>Welcome! You can manage your stores here.</p>
+      <p>
+        Welcome{user?.name ? `, ${user.name}` : ""}! You can manage your stores here.
+      </p>
 
-      <Link to="/owner/stores"
+      <Link
+        to="/owner/stores"
         style={{ color: "blue", marginTop: "20px", display: "inline-block" }}
       >
         View My Stores →
       </Link>
+      <br />  <br />
+
+      
     </div>
   );
 }
+
